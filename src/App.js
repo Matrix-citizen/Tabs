8bfc3de367d2beae0ef8cebe13c02ca7608da43e
Rostyslav <rklyba@yahoo.com>
2021-02-02 08:48:04 +0200

    Configured displaying first job

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
 
-const url = 'https://course-api.com/react-tabs-project'
+const url = "https://course-api.com/react-tabs-project";
 function App() {
   const [loading, setLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
@@ -23,11 +23,35 @@ function App() {
       <section className="section loading">
         <h1>loading...</h1>
       </section>
-    )
+    );
   }
+  const { company, dates, duties, title } = jobs[value];
 
-  
-  return <h2>jobs</h2>
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        {/* btn container */}
+        {/* job info */}
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+    </section>
+  );
 }
 
-export default App
+export default App;
